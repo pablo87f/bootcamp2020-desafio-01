c6fb8d8f69f4fb1c424ca760495c26f6513149bd
Pablo Fernandes <pablofern87@gmail.com>
2020-01-28 23:49:37 -0300

    feat(tasks): - Rota de criação de task

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,6 +1,18 @@
 
+const DataStorage = require('../data-storage')
+
 module.exports = {
     async store(req, res) {
+        const { id } = req.params
+        const { title } = req.body //task title
 
+        req.project.tasks.push(title)
+
+        const updatedProject = await DataStorage.updateById(
+            DataStorage.Entities.Projects,
+            id,
+            req.project
+        )
+        return res.send(updatedProject)
     },
 }
