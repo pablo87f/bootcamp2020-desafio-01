afcfd15ab2b2dd556766c9ef18ec7754f35f7868
Pablo Fernandes <pablofern87@gmail.com>
2020-01-28 23:18:25 -0300

    refactor: - renomeando a pasta de dados armazenados

--- src/data-storage.js
+++ src/data-storage.js
@@ -5,7 +5,7 @@ let DataStorage = {}
 let localStorage = null
 
 if (typeof localStorage === "undefined" || localStorage === null) {
-    localStorage = new LocalStorage('../storage-data');
+    localStorage = new LocalStorage('../stored-data');
 }
 
 // -----------------------------------------
