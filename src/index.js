6b130fca30b774d466884df0affad2fc91df2b8a
Pablo Fernandes <pablofern87@gmail.com>
2020-01-29 00:01:29 -0300

    feat(middlewares): - Criação de middleware que contabiliza as requisições

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const http = require('http')
 const routes = require('./routes')
+const Middlewares = require('./middlewares')
 
 const app = express();
+app.use(Middlewares.requestCouter)
 const server = http.Server(app)
 
 
--- src/middlewares.js
+++ src/middlewares.js
@@ -2,14 +2,15 @@
 const DataStorage = require('./data-storage')
 
 let Middlewares = {}
+let requestCount = 0
 
 Middlewares.checkProjectExists = (req, res, next) => {
     const { id } = req.params
-    
+
     const project = DataStorage.findById(DataStorage.Entities.Projects, id)
-    
-    if(!project) return res.status(404).send()
-    
+
+    if (!project) return res.status(404).send()
+
     req.project = project
 
     return next()
@@ -18,8 +19,16 @@ Middlewares.checkProjectExists = (req, res, next) => {
 Middlewares.checkRequiredProjectFields = (req, res, next) => {
     const { id, title } = req.body
 
-    if (!id) return res.status(401).json({ error:'id is required'})
-    if (!title) return res.status(401).json({ error:'title is required'})
+    if (!id) return res.status(401).json({ error: 'id is required' })
+    if (!title) return res.status(401).json({ error: 'title is required' })
+
+    return next()
+}
+
+
+Middlewares.requestCouter = (req, res, next) => {
+    requestCount++
+    console.log(`Request count: ${requestCount}`)
 
     return next()
 }
